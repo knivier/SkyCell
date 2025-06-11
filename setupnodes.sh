56f1888d48a669a95fc3cfd60bee65d15eb66f60
Patcybermind <pato.gonzalezcabrera@gmail.com>
2025-06-10 22:06:57 -0400

    afd

--- setupnodes.sh
+++ setupnodes.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-meshtastic --set lora.use_preset false --set location_reporting_enabled false --set telemetry_enabled false --set hop_limit 3 --ch-index 0 --ch-set psk "" --set lora.bandwidth 62 --set lora.spread_factor 12 --set lora.coding_rate 8 
+# check if nodes have txco
+meshtastic --set lora.use_preset false --set location_reporting_enabled false --set telemetry_enabled false --set hop_limit 3 --ch-index 0 --ch-set psk "" --set lora.bandwidth 31 --set lora.spread_factor 12 --set lora.coding_rate 8 
